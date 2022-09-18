43f57a8fecdb0ad3eeb315e24a933b53727dceab
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-18 22:27:27 +0300

    Setting up seller_controller.rb

---
+++ app/controllers/seller_controller.rb
@@ -0,0 +1,20 @@
+class SellerController < Sinatra::Base
+    set :default_content_type, 'application/json'
+
+    #TODO:
+    #GET
+    get "/" do
+    end
+
+    #POST
+    post "/" do
+    end
+
+    #PATCH
+    patch "/" do
+    end
+
+    #DELETE
+    delete "/" do
+    end
+end
