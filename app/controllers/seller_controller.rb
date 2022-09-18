246a056050459c8618b5d06063e9310489fd2b5d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-18 23:32:05 +0300

    GET, POST, PATCH, DELETE sellers

--- app/controllers/seller_controller.rb
+++ app/controllers/seller_controller.rb
@@ -2,19 +2,40 @@ class SellerController < Sinatra::Base
     set :default_content_type, 'application/json'
 
     #TODO:
+
     #GET
-    get "/" do
+    get "/sellers" do
+        sellers = Seller.all.order(:created_at)
+        sellers.to_json
     end
 
     #POST
-    post "/" do
+    post "/sellers" do
+        seller = Seller.create(
+            seller_name: params[:seller_name],
+            email: params[:email],
+            password: params[:password]
+        )
+        seller.to_json
     end
 
     #PATCH
-    patch "/" do
+    patch "/sellers/:id" do
+        seller = Seller.find(params[:id])
+        seller.update(
+            seller_name: params[:seller_name],
+            email: params[:email],
+            password: params[:password]
+        )
+        seller.to_json
     end
 
     #DELETE
-    delete "/" do
+    delete "/sellers/:id" do
+        seller = Seller.find(params[:id])
+
+        seller.destroy
+
+        seller.to_json
     end
 end
