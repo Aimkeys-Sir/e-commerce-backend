b86a6f65814fe26c26cf01ac365cccb91aaf9b15
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-18 21:51:50 +0300

    feat: add buyer controller

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,25 @@
+class BuyerController < Sinatra::Base
+    set: default_content_type, 'application/json'
+
+    get '/buyers' do
+        buyers = Buyer.all
+        buyers.to_json
+    end
+
+    get '/buyers/:id' do
+        buyer = Buyer.find(params[:id])
+        buyer.to_json
+    end
+
+    get '/buyers/:id/purchases' do
+        buyer = Buyer.find(params[:id])
+        purchases = buyer.purchases
+        purchases.to_json
+    end
+
+    get '/buyers/:id/products' do
+        buyer = Buyer.find(params[:id])
+        products = buyer.products
+        products.to_json
+    end
+end
