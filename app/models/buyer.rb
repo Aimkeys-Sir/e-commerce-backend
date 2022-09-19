a34fb4a2b35ec74083aecd5e5f729af201044545
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-20 00:16:13 +0300

    Revert "buyer model relationships"
    This reverts commit 26eea4a47c43ce86b98a49b2c5ede022d7ec6475.

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,8 +1,3 @@
 class Buyer < ActiveRecord::Base
     has_many :purchases
-    has_many :reviews
-    has_many :ratings
-    has_many :products, through: :purchases
-    has_many :products, through: :reviews
-    has_many :products, through: :ratings
 end
