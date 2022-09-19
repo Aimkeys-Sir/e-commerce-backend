4e806eaed72cbe8d481d4db13c9c0057fd9fd424
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-19 21:22:21 +0300

    feat:buyer controller full CRUD

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -30,4 +30,79 @@ class BuyersController < Sinatra::Base
         product = Product.find(params[:product_id])
         product.to_json
     end
+
+    post '/buyers' do
+        buyer = Buyer.create(
+            buyer_name: params[:buyer_name],
+            email: params[:email],
+            password: params[:password]
+        )
+        buyer.to_json
+    end
+
+    post '/buyers/:id/purchases' do
+        purchase = Purchase.create(
+            buyer_id: params[:id],
+            seller_id: params[:seller_id],
+            total: params[:total]
+        )
+        purchase.to_json
+    end
+
+    post '/buyers/:id/purchases/:purchase_id/products' do
+        product = Product.create(
+            purchase_id: params[:purchase_id],
+            product_name: params[:product_name],
+            price: params[:price]
+        )
+        product.to_json
+    end
+
+    patch '/buyers/:id' do
+        buyer = Buyer.find(params[:id])
+        buyer.update(
+            buyer_name: params[:buyer_name],
+            email: params[:email],
+            password: params[:password]
+        )
+        buyer.to_json
+    end
+
+    patch '/buyers/:id/purchases/:purchase_id' do
+        purchase = Purchase.find(params[:purchase_id])
+        purchase.update(
+            buyer_id: params[:id],
+            seller_id: params[:seller_id],
+            total: params[:total]
+        )
+        purchase.to_json
+    end
+
+    patch '/buyers/:id/purchases/:purchase_id/products/:product_id' do
+        product = Product.find(params[:product_id])
+        product.update(
+            purchase_id: params[:purchase_id],
+            product_name: params[:product_name],
+            price: params[:price]
+        )
+        product.to_json
+    end
+
+    delete '/buyers/:id' do
+        buyer = Buyer.find(params[:id])
+        buyer.destroy
+        buyer.to_json
+    end
+
+    delete '/buyers/:id/purchases/:purchase_id' do
+        purchase = Purchase.find(params[:purchase_id])
+        purchase.destroy
+        purchase.to_json
+    end
+
+    delete '/buyers/:id/purchases/:purchase_id/products/:product_id' do
+        product = Product.find(params[:product_id])
+        product.destroy
+        product.to_json
+    end
 end
