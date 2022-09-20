aa71edbd232a189ee01f34bda948cb78a186f266
+ <+>
2022-09-20 21:19:28 +0300

    updating conflicts

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,8 +1,15 @@
 class Buyer < ActiveRecord::Base
     has_many :purchases
+<<<<<<< HEAD
     has_many :reviews
     has_many :ratings
     has_many :products, through: :purchases
     has_many :products, through: :reviews
     has_many :products, through: :ratings
 end
+=======
+    has_many :ratings
+    has_many :reviews
+    has_many :products, through: :purchases
+end
+>>>>>>> ce49c30 (updating migration)
